3593e1e82d2f2c1c56a452ce950ad0707d38b779
oaeqo <wjdals7898@naver.com>
2025-02-10 14:31:21 +0900

    ima

--- src/assets/products.js
+++ src/assets/products.js
@@ -1,8 +1,8 @@
 export const products = [
-  {id:1, name:'피자', price:20000, image:'./public/images/img-1.png',category:'Best'},
-  {id:2, name:'햄버거', price:8000, image:'./public/images/img-2.png',category:'New'},
-  {id:3, name:'콜라', price:3000, image:'./public/images/img-3.png',category:'세트'},
-  {id:4, name:'감자튀김', price:5000, image:'./public/images/img-4.png',category:'음료'},
-  {id:5, name:'우유', price:4000, image:'./public/images/img-5.png',category:'디저트'}
+  {id:1, name:'피자', price:20000, image:'./images/img-1.png',category:'Best'},
+  {id:2, name:'햄버거', price:8000, image:'./images/img-2.png',category:'New'},
+  {id:3, name:'콜라', price:3000, image:'./images/img-3.png',category:'세트'},
+  {id:4, name:'감자튀김', price:5000, image:'./images/img-4.png',category:'음료'},
+  {id:5, name:'우유', price:4000, image:'./images/img-5.png',category:'디저트'}
 ];
 export const categories = ['전체','Best','New','세트','음료','디저트'];
